eed5c8250b0047a59a8dcb545ddec83b3e927173
pheonixlegend <jpnc2014@gmail.com>
2018-10-05 12:11:26 +0200

    Coded the selectors and random number generator from 1 to 6

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+
+console.log(dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //Display the result
+
+
+    //Update round score but only if the rolled number was not 1
+})
