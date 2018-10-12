3d2303f2cc2856c094aed7e8d784e5fc39e8f16c
pheonixlegend <jpnc2014@gmail.com>
2018-10-12 22:43:19 +0200

    dom manipulation with random number rolled and setting values to 0 at the start of the gaame

--- app.js
+++ app.js
@@ -16,20 +16,28 @@ roundScore = 0;
 activePlayer = 0;
 
 
-console.log(dice);
+// console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     //Random Number
     var dice = Math.floor(Math.random() * 6) + 1;
 
     //Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
 
     //Update round score but only if the rolled number was not 1
